495b24187f9b480ecb5cb4dada4617cca3c39eb8
naveen <adith.naveen@gmail.com>
2016-10-21 15:22:22 -0700

    initial version for HelloWorld

---
+++ naveen/Testing.java
@@ -0,0 +1,13 @@
+package com.fannie;
+
+
+public class Testing{
+	public static int add(int a, int b){
+		return a + b;
+	}
+	public static void main(String args[]){
+		System.out.println("Hello World in GIT");
+		System.out.print("Addition is " + 
+			add(10, 30));
+	}
+}
